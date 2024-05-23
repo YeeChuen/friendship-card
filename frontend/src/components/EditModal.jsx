2c3fafade9321723df51792e34c09716c64d3cd0
YeeChuen <tychuen98@gmail.com>
2024-05-23 02:33:40 -0500

    option to update gender

--- frontend/src/components/EditModal.jsx
+++ frontend/src/components/EditModal.jsx
@@ -1,97 +1,130 @@
 import {
-	Button,
-	Flex,
-	FormControl,
-	FormLabel,
-	IconButton,
-	Input,
-	Modal,
-	ModalBody,
-	ModalCloseButton,
-	ModalContent,
-	ModalFooter,
-	ModalHeader,
-	ModalOverlay,
-	Textarea,
-	useDisclosure,
-	useToast,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  IconButton,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Textarea,
+  RadioGroup,
+  Radio,
+  useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { BiEditAlt } from "react-icons/bi";
 
-function EditModal({ user, handleUpdate}) {
-	const { isOpen, onOpen, onClose } = useDisclosure();
-	const [inputs, setInputs] = useState({
-		name: user.name,
-		role: user.role,
-		description: user.description,
-	});
-	const toast = useToast();
+function EditModal({ user, handleUpdate }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [inputs, setInputs] = useState({
+    name: user.name,
+    role: user.role,
+    description: user.description,
+    gender: user.gender,
+  });
+  const toast = useToast();
 
-    const handleEdit = async () => {
-        handleUpdate(user.id, inputs)
-    }
+  const handleEdit = async () => {
+    console.log(inputs)
+    handleUpdate(user.id, inputs);
+  };
 
-	return (
-		<>
-			<IconButton
-				onClick={onOpen}
-				variant='ghost'
-				colorScheme='blue'
-				aria-label='See menu'
-				size={"sm"}
-				icon={<BiEditAlt size={20} />}
-			/>
+  return (
+    <>
+      <IconButton
+        onClick={onOpen}
+        variant="ghost"
+        colorScheme="blue"
+        aria-label="See menu"
+        size={"sm"}
+        icon={<BiEditAlt size={20} />}
+      />
 
-			<Modal isOpen={isOpen} onClose={onClose}>
-				<ModalOverlay />
-				<form onSubmit={() => handleEdit()}>
-					<ModalContent>
-						<ModalHeader>Update BFF 🤔</ModalHeader>
-						<ModalCloseButton />
-						<ModalBody pb={6}>
-							<Flex alignItems={"center"} gap={4}>
-								<FormControl>
-									<FormLabel>Full Name</FormLabel>
-									<Input
-										placeholder='John Doe'
-										value={inputs.name}
-										onChange={(e) => setInputs((prev) => ({ ...prev, name: e.target.value }))}
-									/>
-								</FormControl>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <form onSubmit={() => handleEdit()}>
+          <ModalContent>
+            <ModalHeader>Update BFF 🤔</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <Flex alignItems={"center"} gap={4}>
+                <FormControl>
+                  <FormLabel>Full Name</FormLabel>
+                  <Input
+                    placeholder="John Doe"
+                    value={inputs.name}
+                    onChange={(e) =>
+                      setInputs((prev) => ({ ...prev, name: e.target.value }))
+                    }
+                  />
+                </FormControl>
 
-								<FormControl>
-									<FormLabel>Role</FormLabel>
-									<Input
-										placeholder='Software Engineer'
-										value={inputs.role}
-										onChange={(e) => setInputs((prev) => ({ ...prev, role: e.target.value }))}
-									/>
-								</FormControl>
-							</Flex>
-							<FormControl mt={4}>
-								<FormLabel>Description</FormLabel>
-								<Textarea
-									resize={"none"}
-									overflowY={"hidden"}
-									placeholder="He's a software engineer who loves to code and build things."
-									value={inputs.description}
-									onChange={(e) => setInputs((prev) => ({ ...prev, description: e.target.value }))}
-								/>
-							</FormControl>
-						</ModalBody>
+                <FormControl>
+                  <FormLabel>Role</FormLabel>
+                  <Input
+                    placeholder="Software Engineer"
+                    value={inputs.role}
+                    onChange={(e) =>
+                      setInputs((prev) => ({ ...prev, role: e.target.value }))
+                    }
+                  />
+                </FormControl>
+              </Flex>
+              <FormControl mt={4}>
+                <FormLabel>Description</FormLabel>
+                <Textarea
+                  resize={"none"}
+                  overflowY={"hidden"}
+                  placeholder="He's a software engineer who loves to code and build things."
+                  value={inputs.description}
+                  onChange={(e) =>
+                    setInputs((prev) => ({
+                      ...prev,
+                      description: e.target.value,
+                    }))
+                  }
+                />
+              </FormControl>
+              <RadioGroup mt={4} value={inputs.gender}>
+                <Flex gap={5}>
+                  <Radio
+                    value="male"
+                    onChange={(e) => {
+                      setInputs({ ...inputs, gender: e.target.value });
+                    }}
+                  >
+                    Male
+                  </Radio>
+                  <Radio
+                    value="female"
+                    onChange={(e) => {
+                      setInputs({ ...inputs, gender: e.target.value });
+                    }}
+                  >
+                    Female
+                  </Radio>
+                </Flex>
+              </RadioGroup>
+            </ModalBody>
 
-						<ModalFooter>
-							<Button colorScheme='blue' mr={3} type='submit'>
-								Update
-							</Button>
-							<Button onClick={onClose}>Cancel</Button>
-						</ModalFooter>
-					</ModalContent>
-				</form>
-			</Modal>
-		</>
-	);
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} type="submit">
+                Update
+              </Button>
+              <Button onClick={onClose}>Cancel</Button>
+            </ModalFooter>
+          </ModalContent>
+        </form>
+      </Modal>
+    </>
+  );
 }
 
 export default EditModal;
